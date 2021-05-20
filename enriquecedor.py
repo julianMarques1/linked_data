f3e2d7ed9aaddbb04eba3c47027c434ebf5552d2
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-05-20 19:00:51 -0300

    feat: querys dbpedia

--- enriquecedor.py
+++ enriquecedor.py
@@ -1,17 +1,46 @@
 from rdflib import Graph, Literal
+from rdflib.namespace import OWL
+from urllib.request import urlopen, Request
 
-def ah_the_negociator():
-    pass
 
-graphinho = Graph()
-graphinho.parse("links.ttl", format="turtle")
+def get_link_to_data(resource):
+    "Returns 'https://dbpedia.org/data/...'"
 
-for statequieto in graphinho:
-    if not isinstance(statequieto[2], Literal):
-        continue
-    #link_a_dpedia = statequieto[2].toPython()
-    #toa_la_data = ah_the_negociator(link_a_dpedia)
-    dbpedia = Graph()
-    dbpedia.parse("https://dbpedia.org/resource/Aaron_Taylor-Johnson")
-    print(dbpedia.serialize(format="turtle"))
-    break
+    return urlopen(
+        Request(
+            resource,
+            headers={
+                "Accept": "application/rdf+xml",
+            },
+        )
+    )
+
+
+def main():
+    links = Graph()
+    links.parse("links.ttl", format="turtle")
+
+    output = Graph()
+    output.parse("dataset-original.ttl", format="turtle")
+    output.parse("links.ttl", format="turtle")
+
+    proprieties = {}
+
+    for stmt in links:
+        if stmt[1] != OWL["sameAs"]:
+            continue
+
+        actor = Graph()
+        actor.parse(get_link_to_data(stmt[2].toPython()))
+
+        # output.add((individual, OWL.birthDate, actor[aca va el nombre de la propiedad]))
+        # output.add((individual, OWL.propiedad, dbpedia[aca va el nombre de la propiedad]))
+        # output.add((individual, OWL.propiedad, dbpedia[aca va el nombre de la propiedad]))
+
+    output.serialize(
+    "dataset-enriquecido.rdf", encoding="utf-8"
+    )
+
+
+if __name__ == "__main__":
+    main()
