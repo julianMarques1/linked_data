2d6cf82ed180f41f16ea9d48e8194741724f1ec2
Julian Marques de Abrantes <50494011+julianMarques1@users.noreply.github.com>
2021-05-20 14:40:31 -0300

    Empieza co el graphinho

--- enriquecedor.py
+++ enriquecedor.py
@@ -1,8 +1,17 @@
-import rdflib as r
+from rdflib import Graph, Literal
 
-with ("output.ttl") as file:
-    dataser_original = file
+def ah_the_negociator():
+    pass
 
-with ("links.ttl") as file:
-    new_dataset = file
+graphinho = Graph()
+graphinho.parse("links.ttl", format="turtle")
 
+for statequieto in graphinho:
+    if not isinstance(statequieto[2], Literal):
+        continue
+    #link_a_dpedia = statequieto[2].toPython()
+    #toa_la_data = ah_the_negociator(link_a_dpedia)
+    dbpedia = Graph()
+    dbpedia.parse("https://dbpedia.org/resource/Aaron_Taylor-Johnson")
+    print(dbpedia.serialize(format="turtle"))
+    break
